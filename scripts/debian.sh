51a19c728bcf7e12cd51eb79777aaf07654d8dd8
Gorka <gorka@aquigorka.com>
2017-05-07 16:22:41 +0200

    Adds setup script for debian based systems

---
+++ scripts/debian.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Setup script for debian based systems
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/AquiGorka/dotfiles/scripts/debian.sh)"
+# source ./debian.sh
+
+
+# helpers
+check_install() {
+  if [[ ! $(which $@) ]]; then
+    echo "Installing $@"
+    apt-get install $@ -y
+  fi
+}
+# ---
+
+# asume bare bones box
+apt-get update
+
+# if needed ask right away
+check_install sudo
+if [[ $(which sudo) ]]; then
+  sudo -v
+fi
+
+# dependencies
+check_install curl
+check_install git
+check_install python
+check_install zsh
+check_install vim
+
+# oh-my-zsh
+if [[ ! -d ~/.oh-my-zsh/ ]]; then
+  echo "Installing Oh My Zsh"
+  sh -c "$(curl -kfsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# dotfiles
+if [[ ! -d $(pwd)/dotfiles ]]; then
+  git clone https://github.com/AquiGorka/dotfiles
+fi
+./dotfiles/install
+
+# zsh default shell
+if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+  chsh -s $(which zsh)
+fi
+
+# voilá
+source ~/.zshrc
+zsh
