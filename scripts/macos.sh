579e7e21421187e467bbf9ddfd91fd34a4bf3b1e
Gorka <gorka@aquigorka.com>
2017-09-09 15:40:09 +0200

    Macos: set correct path for vim temp folder

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -67,7 +67,7 @@ fi
 # plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # tmp
-mkdir .vim/tmp
+mkdir ~/.vim/tmp
 # todo
 # install plugins
 # :PlugInstall
