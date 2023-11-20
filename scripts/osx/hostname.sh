edd99b5da71954667f20758a0edcd68d2babff30
Gorka <gorka@aquigorka.com>
2023-11-20 09:43:29 -0300

    scripts: add independent script to rename box

---
+++ scripts/osx/hostname.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# change hostname
+scutil --set ComputerName $@
+scutil --set LocalHostName $@
+scutil --set HostName $@
+dscacheutil -flushcache
