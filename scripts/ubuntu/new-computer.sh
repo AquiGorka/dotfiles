6219df97602ce1d2fe02a7db8311eac5775f0ff1
Gorka <gorka@aquigorka.com>
2024-06-20 20:41:46 -0300

    Add insall scripts for new ubuntu computer and user

---
+++ scripts/ubuntu/new-computer.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+sudo apt update
+
+# tools
+sudo apt install -y \
+  tmux \
+  zsh \
+  tree \
+  ripgrep \
+  fzf \
+  vim \
+  fd-find \
+  kitty \
+  golang-go
+
+if [ ! -d ~/.oh-my-zsh ]; then
+  echo "- Installing omz"
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# nvm
+if [ ! -d ~/.nvm ]; then
+  echo "- Installing nvm "
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.7/install.sh | bash
+fi
+
+# docker
+if [[ ! $(which docker) ]]; then
+  echo "- Installing docker"
+  apt install ca-certificates curl
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  chmod a+r /etc/apt/keyrings/docker.asc
+
+  # Add the repository to apt sources:
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  apt update
+  apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  curl -fsSL https://get.docker.com/rootless | sh
+fi
---
+++ scripts/ubuntu/new-user.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# omz
+if [ ! -d ~/.oh-my-zsh ]; then
+  echo "- Installing omz"
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# symlink
+ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/tmux ~/.tmux.conf
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/gitignore.global ~/.gitignore.global
+ln -sf ~/dotfiles/kitty.conf ~/.config/kitty/kitty.conf
+mkdir -p ~/.vim/tmp
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+
+# kitty
+git clone https://github.com/connorholyday/nord-kitty ~/.config/kitty/nord-kitty
+
+# tmux tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  # run tmux then
+  # Press prefix + I (capital i, as in Install) to fetch the plugins
+
+# vim
+  # plugin manager
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  # tmp
+  mkdir -R ~/.vim/tmp
+  # install plugins
+ vim +PlugInstall +qall
+
+# nvm
+if [ ! -d ~/.nvm ]; then
+  echo "- Installing nvm "
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.7/install.sh | bash
+fi
+
+# zsh
+if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+  # add to list of shells
+  sudo sh -c "echo $(which zsh) >> /etc/shells"
+  # default
+  chsh -s $(which zsh)
+  # voilá
+  zsh
+fi
