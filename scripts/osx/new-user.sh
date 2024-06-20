9e267eac49fb9632dc582294fe6c6abd1b0d27b5
Gorka <gorka@aquigorka.com>
2024-06-20 20:39:22 -0300

    new user: remove dotbot and manually link files

--- scripts/osx/new-user.sh
+++ scripts/osx/new-user.sh
@@ -19,7 +19,13 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
   # Press prefix + I (capital i, as in Install) to fetch the plugins
 
 # symlink
-~/dotfiles/symlink
+ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/zshrc.macos ~/.zshrc.macos
+ln -sf ~/dotfiles/tmux ~/.tmux.conf
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/gitignore.global ~/.gitignore.global
+mkdir -R ~/.vim/tmp
+ln -sf ~/dotfiles/vimrc ~/.vimrc
 
 # vim
   # plugin manager
