63dff7200f0827fef2877b30a51ca40c8521f7b8
Gorka <gorka@aquigorka.com>
2024-06-28 23:20:30 -0300

    new computer: use script to install docker

--- scripts/ubuntu/new-computer.sh
+++ scripts/ubuntu/new-computer.sh
@@ -22,14 +22,6 @@ fi
 # docker
 if [[ ! $(which docker) ]]; then
   echo "- Installing docker"
-  apt install ca-certificates curl
-  install -m 0755 -d /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-  chmod a+r /etc/apt/keyrings/docker.asc
-
-  # Add the repository to apt sources:
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  apt update
-  apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-  curl -fsSL https://get.docker.com/rootless | sh
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh ./get-docker.sh
 fi
