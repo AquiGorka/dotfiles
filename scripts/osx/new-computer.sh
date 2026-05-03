0c807648d1ba0667cfd143344a90dbc61e1b6baf
Gorka <gorka@aquigorka.com>
2026-05-03 12:06:12 -0300

    fix(new-computer): use $HOME for brew shellenv zprofile path

--- scripts/osx/new-computer.sh
+++ scripts/osx/new-computer.sh
@@ -7,7 +7,7 @@ sudo -v
 if [[ ! $(which brew) ]]; then
   echo "Installing hombrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/gorka/.zprofile
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> "$HOME/.zprofile"
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
